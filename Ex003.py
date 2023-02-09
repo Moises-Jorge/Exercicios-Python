6c04c2ca15b4499310030406c3452dc131e1c0a5
Moisés Jorge <moisesjorge30@gmail.com>
2023-02-09 22:44:04 +0100

    Create Ex003.py
    Programa que lê o dia, mês e ano de uma pessoa e mostra uma mensagem com a data formatada.

---
+++ Ex003.py
@@ -0,0 +1,7 @@
+print("Digite sua data de nascimento\n")
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+
+print("Vc nasceu no dia {} de {} de {}. Correto?".format(dia, mes, ano))
+
