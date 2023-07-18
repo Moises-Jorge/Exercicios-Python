bfa0e8f1b9231905b2dfd931303a53ca28656362
Moisés Jorge <69114568+Moises-Jorge@users.noreply.github.com>
2023-07-18 04:49:27 +0100

    Atualizar o Ex033.py
    Melhorei o código!

--- Ex033.py
+++ Ex033.py
@@ -4,17 +4,18 @@ n1 = int(input("N1: "))
 n2 = int(input("N2: "))
 n3 = int(input("N3: "))
 
-if(n1 >= n2) and (n1 >= n3) and (n2 > n3):
-    maior = n1
+# Verificando o menor número 
+menor = n1
+if (n2 < n1) and (n2 < n3):
     menor = n3
-elif(n2 >= n1) and (n2 >= n3) and (n1 > n3):
+if (n3 < n1) and (n3 < n2):
+    menor = n3
+
+# Verificando o maior número 
+maior = n1
+if (n2 > n1) and (n2 > n3):
     maior = n2
-    menor = n3
-elif(n3 >= n1) and (n3 >= n2) and (n1 > n2):
+if (n3 > n1) and (n3 > n2):
     maior = n3
-    menor = n2
-elif(n3 >= n1) and (n3 >= n2) and (n2 > n1):
-    maior = n3
-    menor = n1
 
 print("Todos os numeros sao iguais" if(n1 == n2 == n3) else "Maior Numero: {}\nMenor Numero: {}".format(maior, menor))
