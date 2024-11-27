4c21a3a61d5eac5670b1bc825d3c11d54cc5f6ed
Moisés Jorge <moisesjorge30@gmail.com>
2024-11-27 01:23:23 +0100

    updated Ex042

--- Ex042.py
+++ Ex042.py
@@ -8,9 +8,9 @@ b = float(input('Informe a segunda recta: '))
 c = float(input('Informe a terceira recta: '))
 
 if (a + b > c) and (a + c > b) and (b + c > a):
-    if a == b and b == c:
+    if a == b and b == c: # Poderia ser da seguinte forma: a == b == c ...
         print('Essas rectas podem formar um triangulo: Equilatero')
-    elif a != b and b != c and a != c:
+    elif a != b and b != c and a != c: # Poderia ser da seguinte forma: a != b != c != a ...
         print('Essas rectas podem formar um triangulo: Escaleno')
     else:
         print('Essas rectas podem formar um triangulo: Isosceles')
