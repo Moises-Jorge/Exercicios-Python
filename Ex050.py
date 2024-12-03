9345cf53b8f825ad54eb51206d5b4a888adc0c44
Moisés Jorge <moisesjorge30@gmail.com>
2024-12-03 12:39:51 +0100

    update Ex050

--- Ex050.py
+++ Ex050.py
@@ -1,11 +1,8 @@
 ''' Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daquelas que forem pares. Se o valor digitado for ímpar, desconsidere-o. '''
 
-lista_num = []
 soma = 0
-
 for i in range(6):
     numero = int(input(f'Digite o {i+1} numero: '))
-    lista_num.append(numero)
     if (numero % 2 == 0):
         soma += numero    
 print(f'\nSoma dos pares: {soma}')
