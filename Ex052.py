2b875a9a1ea0035f38f26d9c81acb9932da46c77
Moisés Jorge <moisesjorge30@gmail.com>
2024-12-03 14:39:18 +0100

    update Ex052

--- Ex052.py
+++ Ex052.py
@@ -1,8 +1,12 @@
 ''' Faça um programa que leia um número inteiro e diga se ele é ou não um número primo. '''
 numero = int(input('Digite o numero: '))
+tot_divisores = 0
 
-if numero <= 1:
-    print(f'{numero} nao eh primo')
+for c in range(1, numero + 1): 
+    if numero % c == 0:
+        tot_divisores += 1
+
+if tot_divisores == 2: # Um numero PRIMO soh tem 2 divisores: 1 e ele mesmo
+    print(f'{numero} eh PRIMO')
 else:
-    primo = True
-    
+    print(f'{numero} NAO eh PRIMO')
