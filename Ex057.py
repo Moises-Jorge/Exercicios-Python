86e2c61538fed23a77fe1496acc357bc46757fed
Moisés Jorge <moisesjorge30@gmail.com>
2024-12-05 15:54:12 +0100

    update Ex057

--- Ex057.py
+++ Ex057.py
@@ -1,12 +1,17 @@
 ''' Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou F. Caso esteja errado, peça a digitação novamente até ter um valor correto. '''
 
-""" PRIMEIRA LOGICA
-sexo = ''
-while (sexo != 'M') or (sexo != 'F') :
-    sexo = str(input('Digite o sexo: ')).upper() """
+# PRIMEIRA LOGICA
+sexo = str(input('Informe seu sexo [M/F]: ')).upper().strip()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados Invalidos. Por favor, informe seu sexo [M/F]:')).upper().strip()[0]
+print(f'Sexo {sexo} registrado com sucesso')
     
+""" # SEGUNDA LOGICA
 sexo_valido = True
 while sexo_valido:
-    sexo = str(input('Digite o sexo [M/F]: ')).upper()
+    sexo = str(input('Informe seu sexo [M/F]: ')).upper().strip()[0]
     if sexo == 'M' or sexo == 'F':
         sexo_valido = False
+    elif sexo not in 'MF':
+        print('Dados invalidos. Por favor, ', end='')
+print(f'Sexo {sexo} registrado com sucesso') """
