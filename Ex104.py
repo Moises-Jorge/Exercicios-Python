6b6f3c376ec4b604f2b9132af24925f5da10bda8
Moisés Jorge <moisesjorge30@gmail.com>
2025-02-07 11:12:21 +0100

    Add docstring of Ex104: leia_int function

--- Ex104.py
+++ Ex104.py
@@ -1,5 +1,13 @@
 '''Crie um programa que tenha uma função leia_int(), que vai funcionar de forma semelhante à função input() do python, só que fazendo a validação para aceitar apenas valores numéricos'''
 def leia_int(msg_entrada):
+    """-> Funcao que verifica se o valor de entrada eh mesmo um numero inteiro
+
+    Args:
+        msg_entrada (literal): Mensagem de entrada
+
+    Returns:
+        valor inteiro: o valor inteiro lido
+    """
     while True:
         n = str(input(msg_entrada))
         if n.isnumeric():
