f548d6d41dd2cf02b317267f95c3a3b8e655da40
Moisés Jorge <moisesjorge30@gmail.com>
2025-02-12 10:15:51 +0100

    Modified Ex113: adicionei as funcoes criadas nesse exercio no pacote MODULOS e importei a partir de la para o exercicio funcionar

--- Ex113.py
+++ Ex113.py
@@ -1,44 +1,5 @@
 '''Rescreva a função leia_int() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um numero invalido. Aproveite e crie também uma função leia_float() com a mesma funcionalidade.'''
-def leia_int(msg):
-    """ -> Funcao responsavel por ler e validar somente numeros inteiros
-
-    Args:
-        msg (string): mensagem de entrada (que pede para o usuario informar o valor)
-
-    Returns:
-        inteiro: retorna o numero inteiro lido
-    """
-    while True:
-        try:
-            num = int(input(msg))
-        except (ValueError, TypeError):
-            print(f'\033[0;31mERRO: por favor, digite um numero inteiro valido.\033[m')
-        except (KeyboardInterrupt):
-            print('\033[0;Usuario preferiu nao digitar esse numero.\033[m')
-            return 0
-        else:
-            return num
-        
-        
-def leia_float(msg):
-    """-> Funcao responsavel por ler e validar somente numeros com ponto flutuante
-
-    Args:
-        msg (string): mensagem de entrada (que pede para o usuario informar o valor)
-
-    Returns:
-        float: retorna o numero real lido
-    """
-    while True:
-        try:
-            num = float(input(msg))
-        except (ValueError, TypeError):
-            print(f'\033[0;31mERRO: por favor, digite um numero real valido.\033[m')
-        except (KeyboardInterrupt):
-            print('\033[0;Usuario preferiu nao digitar esse numero.\033[m')
-            return 0
-        else:
-            return num
+from modulos.uteis.numeros import leia_int, leia_float
         
 
 
--- modulos/uteis/numeros/__init__.py
+++ modulos/uteis/numeros/__init__.py
@@ -11,3 +11,45 @@ def dobro(n):
 
 def triplo(n):
     return n * 3
+
+
+def leia_int(msg):
+    """ -> Funcao responsavel por ler e validar somente numeros inteiros
+
+    Args:
+        msg (string): mensagem de entrada (que pede para o usuario informar o valor)
+
+    Returns:
+        inteiro: retorna o numero inteiro lido
+    """
+    while True:
+        try:
+            num = int(input(msg))
+        except (ValueError, TypeError):
+            print(f'\033[0;31mERRO: por favor, digite um numero inteiro valido.\033[m')
+        except (KeyboardInterrupt):
+            print('\033[0;Usuario preferiu nao digitar esse numero.\033[m')
+            return 0
+        else:
+            return num
+        
+        
+def leia_float(msg):
+    """-> Funcao responsavel por ler e validar somente numeros com ponto flutuante
+
+    Args:
+        msg (string): mensagem de entrada (que pede para o usuario informar o valor)
+
+    Returns:
+        float: retorna o numero real lido
+    """
+    while True:
+        try:
+            num = float(input(msg))
+        except (ValueError, TypeError):
+            print(f'\033[0;31mERRO: por favor, digite um numero real valido.\033[m')
+        except (KeyboardInterrupt):
+            print('\033[0;Usuario preferiu nao digitar esse numero.\033[m')
+            return 0
+        else:
+            return num
